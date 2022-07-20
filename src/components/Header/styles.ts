ebed3c9757d1ac38e29733f6ca54175b493a287c
Gustavo Bernardo <gustavobtflores@gmail.com>
2022-07-19 21:42:26 -0300

    Adds app routes with react-router-dom; Home and History structure (HTML + CSS) complete;

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  nav {
+    display: flex;
+    gap: 0.5rem;
+
+    a {
+      width: 3rem;
+      height: 3rem;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: ${(props) => props.theme['gray-100']};
+
+      border-top: 3px solid transparent;
+      border-bottom: 3px solid transparent;
+
+      transition: border 0.2s ease;
+
+      &:hover {
+        border-bottom-color: ${(props) => props.theme['green-300']};
+      }
+
+      &.active {
+        color: ${(props) => props.theme['green-500']};
+      }
+    }
+  }
+`
